03fd37a2e87d02e674292601933666e73c5b479a
elior_av <elior.av@gmail.com>
2020-01-11 17:19:04 +0200

    add model_input_size param to the preprocessing script

--- data/image_preprocessor.py
+++ data/image_preprocessor.py
@@ -11,26 +11,28 @@ from constants import MODEL_INPUT_SIZE, DOMAIN_A_FILES, DOMAIN_B_FILES, DOMAIN_A
     DOMAIN_B_READY, DOMAIN_B_FOLDER
 
 
-def preprocess():
+def preprocess(model_input_size):
     dataset_a = glob(DOMAIN_A_FILES)
     dataset_b = glob(DOMAIN_B_FILES)
     dataset_size = min(len(dataset_a), len(dataset_b))
-    preprocess_dataset(dataset_a, DOMAIN_A_FOLDER, DOMAIN_A_READY, dataset_size)
-    preprocess_dataset(dataset_b, DOMAIN_B_FOLDER, DOMAIN_B_READY, dataset_size)
+    preprocess_dataset(dataset_a, DOMAIN_A_FOLDER, DOMAIN_A_READY, dataset_size, model_input_size)
+    preprocess_dataset(dataset_b, DOMAIN_B_FOLDER, DOMAIN_B_READY, dataset_size, model_input_size)
 
 
-def preprocess_image(image_path, domain_folder, domain_ready_folder, pbar):
+def preprocess_image(image_path, domain_folder, domain_ready_folder, model_input_size, pbar):
     img = Image.open(image_path)
-    processed_image = img.resize((MODEL_INPUT_SIZE, MODEL_INPUT_SIZE))
+    processed_image = img.resize((model_input_size, model_input_size))
     processed_image = processed_image.convert('RGB')
     processed_image.save(image_path.replace(domain_folder, domain_ready_folder))
     pbar.update()
 
 
-def preprocess_dataset(dataset_glob, domain_folder, domain_ready_folder, dataset_size):
+def preprocess_dataset(dataset_glob, domain_folder, domain_ready_folder, dataset_size, model_input_size):
     shuffled_data = copy(dataset_glob)
     np.random.shuffle(shuffled_data)
     dataset = shuffled_data[:dataset_size]
     with tqdm(total=len(dataset)) as pbar:
         pool = ThreadPool(multiprocessing.cpu_count())
-        pool.map(lambda image_path: preprocess_image(image_path, domain_folder, domain_ready_folder, pbar), dataset)
+        pool.map(
+            lambda image_path: preprocess_image(image_path, domain_folder, domain_ready_folder, model_input_size, pbar),
+            dataset)
--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,6 @@
-from constants import DOMAIN_A_READY, DOMAIN_B_READY
+import argparse
+
+from constants import DOMAIN_A_READY, DOMAIN_B_READY, MODEL_INPUT_SIZE
 from data.image_preprocessor import preprocess
 from services import clear_folder
 
@@ -8,10 +10,14 @@ def prepare_data_ready_folders():
     clear_folder(DOMAIN_B_READY)
 
 
-def main():
+def main(args):
+    model_input_size = args.model_input_size
     prepare_data_ready_folders()
-    preprocess()
+    preprocess(model_input_size)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='arguments for the preprocessing step')
+    parser.add_argument('--model_input_size', default=MODEL_INPUT_SIZE, type=int, help="")
+    args = parser.parse_args()
+    main(args)
